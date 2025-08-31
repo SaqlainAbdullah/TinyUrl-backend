a0698314c804d93b5cee64ea167f9047caac485a
SaqlainAbdullah <saqlainabdullah95@gmail.com>
2025-08-31 12:39:10 -0700

    did changes in redirect file for connecting with frontend

--- Controllers/RedirectURL.js
+++ Controllers/RedirectURL.js
@@ -1,17 +1,19 @@
 import { URLs } from "../Models/url.js";
 
-export const RedirectURL = async (res, req) => {
+export const RedirectURL = async (req, res) => {
   const { shortId } = req.params;
   try {
-    const resUrls = await URLs.find({ _id: shortId });
-    const element = resUrls[0];
-    console.log(element);
+    const urlDoc = await URLs.findById(shortId);
+    if (!urlDoc) {
+      return res.status(404).json({ ok: false, error: "URL not found" });
+    }
 
-    res.redirect(element.longUrl);
+    return res.redirect(urlDoc.longUrl);
   } catch (error) {
     console.log(error);
     res.status(500).json({
       ok: false,
+      error: "Error during redirect",
     });
   }
 };
