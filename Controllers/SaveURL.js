9ca9495d7f178a263841b62aa4211012ae948107
SaqlainAbdullah <saqlainabdullah95@gmail.com>
2025-08-31 12:38:15 -0700

    changes in saveURL file

--- Controllers/SaveURL.js
+++ Controllers/SaveURL.js
@@ -1,21 +1,25 @@
 import { URLs } from "../Models/url.js";
 
-export const SaveURL = async (res, req) => {
+export const SaveURL = async (req, res) => {
   console.log(req.body);
   const { longUrl } = req.body;
   try {
-    const newURL = new URLs({ longUrl: longUrl });
+    const newURL = new URLs({ longUrl });
     const savedRef = await newURL.save();
+
+    // Use Railway domain here
     const shortURLId = savedRef._id;
-    const shortURL = `http://localhost:5050/url/${shortURLId}`;
+    const shortURL = `https://tinyurl-backend-production.up.railway.app/url/${shortURLId}`;
+
     res.status(200).json({
       ok: true,
-      shortURL: shortURL,
+      shortUrl: shortURL,
     });
   } catch (err) {
     console.log(err);
     res.status(500).json({
       ok: false,
+      error: "Server error while saving URL",
     });
   }
 };
